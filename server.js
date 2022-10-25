74005ab057863266116d3e6d82515d6a0596c2f0
‪Osama Salem‬‏ <70097854+Osama-Same@users.noreply.github.com>
2022-10-25 23:41:04 +0300

    my first commit

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const router = require("./Router/router");
 const bodyParser = require('body-parser')
 require("./connection/connection");
-const sslRedirect = require('heroku-ssl-redirect').default
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
@@ -16,11 +15,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json())
 app.use(router);
-app.use(sslRedirect());
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
+
+
 app.use(express.static(path.resolve(__dirname, "client/build")));
 
 app.listen(PORT, () => {

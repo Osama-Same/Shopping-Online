48d948df49521ce362ccecf1eb3d84320170d0ca
‪Osama Salem‬‏ <70097854+Osama-Same@users.noreply.github.com>
2022-10-25 23:22:23 +0300

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 require("./connection/connection");
 const sslRedirect = require('heroku-ssl-redirect').default
 require("dotenv").config();
-
+const PORT = process.env.PORT || 8000;
 const corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200,
@@ -22,7 +22,7 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 app.use(express.static(path.resolve(__dirname, "client/build")));
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 

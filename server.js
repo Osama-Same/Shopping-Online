755fd98ae77c8ed168e96e7458cb5300d57b6ab0
‪Osama Salem‬‏ <70097854+Osama-Same@users.noreply.github.com>
2022-10-26 17:22:52 +0300

    first commit

--- server.js
+++ server.js
@@ -13,9 +13,14 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router)
 app.use(express.static(path.resolve(__dirname, "client/build")));
-const PORT = process.env.PORT || 5000;
+/* const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
+ */
 
+const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`http://localhost: ${port}`);
+});
 

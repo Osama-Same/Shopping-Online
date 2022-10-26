ef0df778490a2c7ba9267953feca0304912f44b1
‪Osama Salem‬‏ <70097854+Osama-Same@users.noreply.github.com>
2022-10-26 21:02:38 +0300

    changes

--- router/router.js
+++ router/router.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { _getUsers } = require("../controller/users");
+const { _getProducts } = require("../controller/products");
 const router = express.Router();
 router.get("/users", _getUsers);
+router.get("/products", _getProducts);
 module.exports = router;
